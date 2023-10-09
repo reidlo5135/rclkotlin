a6cfb82444e7875e9f11a4ef2d5db54229f63003
reidlo5135 <201840103@email.daelim.ac.kr>
2023-10-09 19:14:23 +0900

    refactor : modify RCLKotlin.kt's function structure to companion objects

--- src/main/kotlin/net/wavem/rclkotlin/rosdds/topic/Publisher.kt
+++ src/main/kotlin/net/wavem/rclkotlin/rosdds/topic/Publisher.kt
@@ -7,23 +7,20 @@ import pinorobotics.rtpstalk.RtpsTalkClient
 import pinorobotics.rtpstalk.RtpsTalkConfiguration
 import pinorobotics.rtpstalk.messages.RtpsTalkDataMessage
 import java.util.concurrent.SubmissionPublisher
-import kotlin.reflect.KClass
 
-class RCLPublisher<M : Message>(
-    private val messageType : KClass<M>
-) {
-    private val ddsClient : RtpsTalkClient = RtpsTalkClient(
+class Publisher {
+    val ddsClient : RtpsTalkClient = RtpsTalkClient(
         RtpsTalkConfiguration.Builder()
             .networkInterface(DDSSupport.DDS_NETWORK_INTERFACE_TYPE)
             .build()
     )
 
-    private val ddsSupport : DDSSupport = DDSSupport()
-    private val ddsPublisher : SubmissionPublisher<RtpsTalkDataMessage> = SubmissionPublisher<RtpsTalkDataMessage>()
+    val ddsSupport : DDSSupport = DDSSupport()
+    val ddsPublisher : SubmissionPublisher<RtpsTalkDataMessage> = SubmissionPublisher<RtpsTalkDataMessage>()
 
-    fun registerPublisher(topic : String) {
+    inline fun <reified M : Message> registerPublisher(topic : String) {
         val ddsTopic : String = ddsSupport.qualifyTopic(topic)
-        val ddsMessageType : String = ddsSupport.qualifyMessageType(messageType)
+        val ddsMessageType : String = ddsSupport.qualifyMessageType(M::class)
 
         ddsClient.publish(ddsTopic, ddsMessageType, DDSQoS.DEFAULT_PUBLISHER_QOS, ddsPublisher)
     }
